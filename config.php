0ef4e437f4edf889e2f725288a788c86bf0aa60e
wuyow <45787045+wuyow@users.noreply.github.com>
2019-01-17 00:20:50 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyCVkRcSiF0KmLCwmoo4edtw0vnSykCvFcc');
+define('APIKEY', 'AIzaSyCaA7q742-GYbBfC1xyFjSA1UaaJVrTHPQ');
 define('GJ_CODE', 'US');
-define('SITE_NAME', 'y2b');
-define('TITLENAME', 'y2b');
+define('SITE_NAME', 'v2u');
+define('TITLENAME', 'v2u');
 define('EN2DEKEY', '32123wwerwer323tgfdgds');
-define('EMAIL', '1334347212@qq.com');
-define('NAME', 'admin');
-define('PASSWORD', 'admin');
+define('EMAIL', 'admin@admin.com');
+define('NAME', 'vrok');
+define('PASSWORD', 'vrok.tk');
 define('LOGINHTML', '<!DOCTYPE html>
 <html lang="en">
 <head>
